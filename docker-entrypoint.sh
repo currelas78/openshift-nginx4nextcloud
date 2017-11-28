341c44bfc8989d992e0551286c2d501df0f88604
Francisco Cuadrado <curro@qindel.com>
2017-11-28 13:39:22 +0100

    test entrypoint 2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "#upstream php-handler { server ${NEXTCLOUD_HOST}:9000; }" > /etc/nginx/conf.d/upstream.conf
+echo "#upstream php-handler { server ${NEXTCLOUD_HOST}:9000; }" > /tmp/upstream.conf
 
 exec "$@"
